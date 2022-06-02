96dd902ebc570e57712818a415fce95967bcf7ae
Timothy Cates <timothyccates1@gmail.com>
2022-06-02 01:54:05 -0700

    Moved route code to helper file to make routes file easier to read

--- Udemy/Adv Web Dev Bootcamp/Exercises/todos api/routes/todos.js
+++ Udemy/Adv Web Dev Bootcamp/Exercises/todos api/routes/todos.js
@@ -1,57 +1,16 @@
 let express = require('express')
 let router = express.Router();
-let db = require("../models")
+let helper = require("../helpers/todos")
 
 
+router.get('/', helper.getTodos)
 
-router.get('/', (req, res) => {
-    db.Todo.find()
-        .then(todos => {
-            res.json(todos)
-        })
-        .catch(err => {
-            res.send(err)
-        })
-})
+router.post('/', helper.addTodo)
 
-router.post('/', (req, res) => {
-   db.Todo.create(req.body)
-    .then((newTodo) => {
-        res.status(201).json(newTodo)
-    })
-    .catch(err => {
-        res.send(err);
-    })
-})
+router.get('/:todoId', helper.getTodo)
 
-router.get('/:todoId', (req, res) => {
-    db.Todo.findById(req.params.todoId)
-    .then(todo => {
-        res.json(todo)
-    })
-    .catch(err => {
-        res.send(err)
-    })
-})
+router.put('/:todoId', helper.updateTodo)
 
-router.put('/:todoId', (req, res) => {
-    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
-    .then(todo => {
-        res.json(todo)
-    })
-    .catch(err => {
-        res.send(err)
-    })
-})
-
-router.delete('/:todoId', (req, res) => {
-    db.Todo.remove({_id: req.params.todoId})
-    .then(todo => {
-        res.json(todo)
-    })
-    .catch(err => {
-        res.send(err)
-    })
-})
+router.delete('/:todoId', helper.deleteTodo)
 
 module.exports = router
