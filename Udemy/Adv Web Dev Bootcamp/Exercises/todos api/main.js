6fb74c2836de4f7e7f0f6585491843b3bdda8292
Timothy Cates <timothyccates1@gmail.com>
2022-06-06 11:11:13 -0700

    Server can now, respond to requests with static files

--- Udemy/Adv Web Dev Bootcamp/Exercises/todos api/main.js
+++ Udemy/Adv Web Dev Bootcamp/Exercises/todos api/main.js
@@ -4,12 +4,14 @@ let app = express();
 let bodyParser = require('body-parser')
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/views'))
 
 let todoRoutes = require('./routes/todos')
 
 app.get('/', (req, res) => {
-    res.send("The root dir")
+    res.sendFile('index.html');
 })
 
 app.use('/api/todos', todoRoutes);
