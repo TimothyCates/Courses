cb1d2b72661c1cf84660ff944f17e581fb8c5c26
Timothy Cates <timothyccates1@gmail.com>
2022-06-07 13:15:15 -0700

    Render todos

--- Udemy/Adv Web Dev Bootcamp/Exercises/todos api/public/todo.js
+++ Udemy/Adv Web Dev Bootcamp/Exercises/todos api/public/todo.js
@@ -1,1 +1,40 @@
-alert("test")
+const docReady = (fn) => {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+const fetchOpts = (mode) => {
+    return {
+        method: mode,
+        mode: 'same-origin',
+    }
+}
+
+const renderTodos = (todosArr) => {
+    const list = document.getElementsByClassName('list')[0]
+    list.innerHTML = ""
+    todosArr.forEach(todo => {
+        let classNames = ['task']
+        if (todo.completed)
+            classNames.push('done')
+        list.innerHTML += `<li id="${todo._id}" class="${classNames.join(' ')}">
+            ${todo.name}
+            </li>\n`;
+    });
+}
+
+docReady(() => {
+    fetch('/api/todos', fetchOpts)
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            renderTodos(data);
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
