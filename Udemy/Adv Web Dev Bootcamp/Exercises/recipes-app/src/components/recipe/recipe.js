9cafe308c1b01c035b1b8f125e04e8d898e6bacb
Timothy Cates <timothyccates1@gmail.com>
2022-06-10 11:05:38 -0700

    Added css

--- Udemy/Adv Web Dev Bootcamp/Exercises/recipes-app/src/components/recipe/recipe.js
+++ Udemy/Adv Web Dev Bootcamp/Exercises/recipes-app/src/components/recipe/recipe.js
@@ -1,23 +1,28 @@
 import React, { Component } from "react";
+import './recipe.css'
 
 class Recipe extends Component {
-    render () {
-        const {title, img, steps} = this.props;
+    render() {
+        const { title, img, steps } = this.props;
         const ingredients = this.props.ingredients.map((item, index) => (
             <li key={index}>{item}</li>
         ))
         return (
-            <div>
-                <div>
-                   Recipe {title} 
+            <div className="recipe-card">
+                <div className="recipe-card-img">
+                    <img alt={title} src={img} />
                 </div>
-                <img alt={title} src={img}/>
-                <ul>
-                    {ingredients}
-                </ul>
-                <p>
-                    {steps} 
-                </p>
+                <div className="recipe-card-content">
+                    <h3 className="recipe-card-title">Recipe {title}</h3>
+                    <h4>Ingredients:</h4>
+                    <ul>
+                        {ingredients}
+                    </ul>
+                    <h4>Instructions:</h4>
+                    <p>
+                        {steps}
+                    </p>
+                </div>
             </div>
         )
     }
