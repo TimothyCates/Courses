3e38da2e941ebba85e1fa6cd52ccb0a07589a86e
Timothy Cates <timothyccates1@gmail.com>
2022-06-11 11:47:09 -0700

    Added default props and prop-type checking. Added check for image to do a no-zoom when no image is present

--- Udemy/Adv Web Dev Bootcamp/Exercises/recipes-app/src/components/recipe/recipe.js
+++ Udemy/Adv Web Dev Bootcamp/Exercises/recipes-app/src/components/recipe/recipe.js
@@ -1,16 +1,32 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types'
 import './recipe.css'
 
 class Recipe extends Component {
+    static defaultProps = {
+        title: "Unnamed Recipe",
+        img: "./No-Image.svg",
+        ingredients: [],
+        steps: "Fill out the form correctly!"
+    }
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        steps: PropTypes.string.isRequired,
+        ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
+    }
+
     render() {
         const { title, img, steps } = this.props;
         const ingredients = this.props.ingredients.map((item, index) => (
             <li key={index}>{item}</li>
         ))
+        
+        const hasImage = (img !== "./No-Image.svg")
+
         return (
             <div className="recipe-card">
                 <div className="recipe-card-img">
-                    <img alt={title} src={img} />
+                    <img alt={title} src={img} className={hasImage ? "" : "no-zoom"}/>
                 </div>
                 <div className="recipe-card-content">
                     <h3 className="recipe-card-title">Recipe {title}</h3>
