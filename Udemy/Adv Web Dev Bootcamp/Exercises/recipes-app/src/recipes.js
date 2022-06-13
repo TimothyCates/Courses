460a247d3a27664a2245855ee2f9132eeb92250e
Timothy Cates <timothyccates1@gmail.com>
2022-06-13 08:46:36 -0700

    Fill out fake recipe data with, shitty fake recipes

--- Udemy/Adv Web Dev Bootcamp/Exercises/recipes-app/src/recipes.js
+++ Udemy/Adv Web Dev Bootcamp/Exercises/recipes-app/src/recipes.js
@@ -2,50 +2,65 @@ let recipes = [
     {
         title: "Fajitas",
         ingredients: [
-
+            "chicken",
+            "Bell Peppers",
+            "Assorted Seasonings"
         ],
-        steps: "",
-        img: ""
+        steps: "Chip and Saute bell pepers, add chicken, mix in seasonings",
+        img: "https://www.budgetbytes.com/wp-content/uploads/2022/03/Sheet-Pan-Chicken-Fajitas-done.jpg"
     },
     {
         title: "Tacos",
         ingredients: [
-
+            "Beef",
+            "Assorted Seasonings",
+            "Tortillas",
+            "Cheese"
         ],
-        steps: "",
-        img: ""
+        steps: "Brown beef in a pan, mix in assorted seasonings, and cook until done",
+        img: "https://cookingwithcocktailrings.com/wp-content/uploads/2021/11/Birria-Tacos-114.jpg"
     },
     {
-        title: " ",
+        title: "Enchiladas",
         ingredients: [
-
+            "Enchilada sauce",
+            "Beef",
+            "Tortillas",
+            "Assorted Seasonings",
+            "Hot Sauce"
         ],
-        steps: "",
-        img: ""
+        steps: "Create taco beef, create tortillas sprinkling enchilada suace inside. When all are done Layer the top with enchilada sauce and cheese inside an oven pan. Cook for 10-20 mins",
+        img: "https://jandatri.com/wp-content/uploads/2017/08/27-Chicken-Enchiladas.jpg"
     },
     {
-        title: "",
+        title: "Fajita Pasta",
         ingredients: [
-
+            "Chicken",
+            "Penne",
+            "Assorted Seasoning",
+            "Cheese"
         ],
-        steps: "",
-        img: ""
+        steps: "Saute bell peppers in a large pot, add chicken. When cooked add penne pasta and cheese sauce.",
+        img: "https://static.onecms.io/wp-content/uploads/sites/24/2021/07/29/One-Pot-Chicken-Fajita-Pasta228-2000.jpg"
     },
     {
-        title: "",
+        title: "Mac and Cheese",
         ingredients: [
-
+            "Large elbow macaroni",
+            "Velveeta cheese",
+            "Milk"
         ],
-        steps: "",
-        img: ""
+        steps: "Boil large elbow macaroni until tender, add block of Velveeta cheese and milk until covered. Cook until cheesy.",
+        img: "https://insanelygoodrecipes.com/wp-content/uploads/2021/03/Homemade-Cheesy-Mac-and-Cheese.png"
     },
     {
-        title: "",
+        title: "Penne Alfredo",
         ingredients: [
-
+            "Alfredo Sauce",
+            "Penne"
         ],
-        steps: "",
-        img: ""
+        steps: "Mix it all up fam, none of these are real anyway.",
+        img: "https://lmld.org/wp-content/uploads/2013/10/One-Pot-Chicken-Alfredo-Pasta-4-500x375.jpg"
     },
 ]
 
