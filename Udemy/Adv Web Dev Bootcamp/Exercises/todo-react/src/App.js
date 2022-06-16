0976fb6fe9dee538c54438b85a4dfd0e821737c9
Timothy Cates <timothyccates1@gmail.com>
2022-06-15 20:49:56 -0700

    Define the state, state functions, and pass them into props of sub components

--- Udemy/Adv Web Dev Bootcamp/Exercises/todo-react/src/App.js
+++ Udemy/Adv Web Dev Bootcamp/Exercises/todo-react/src/App.js
@@ -1,11 +1,38 @@
 import { Component } from 'react'
+import List from './components/list/list'
+import Form from './components/form/form'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      items: [],
+      inputText: ""
+    }
+  }
+
+  updateInput = (value) => {
+    this.setState({inputText: value})
+  }
+
+  addTodo = (todo) => {
+    this.setState((prev) => {
+      return {items: [...prev.items, todo]}
+    })
+  }
+
   render () {
     return (
-      <div>
-        
-      </div>
+      <>
+        <h1>Todo App - React</h1>
+        <Form 
+          setInput={this.updateInput} 
+          addTodo={this.addTodo} 
+          inputValue={this.state.inputText} 
+          subText="Save"
+        />
+        <List items={this.state.items} />
+      </>
     )
   }
 }
