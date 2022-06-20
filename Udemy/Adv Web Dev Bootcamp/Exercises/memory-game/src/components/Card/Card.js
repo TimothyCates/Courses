46f89959e98d741dd0eac1966b963837e68ef7ff
Timothy Cates <timothyccates1@gmail.com>
2022-06-20 07:46:29 -0700

    Simplified card component

--- Udemy/Adv Web Dev Bootcamp/Exercises/memory-game/src/components/Card/Card.js
+++ Udemy/Adv Web Dev Bootcamp/Exercises/memory-game/src/components/Card/Card.js
@@ -2,19 +2,26 @@ import { Component } from 'react'
 import './Card.css'
 
 class Card extends Component {
+
   frontStyle = {
     backgroundColor: this.props.color,
-    transform: "rotateY(180deg)"
   }
 
-
   render() {
-    let { handleClick, animation, state, id } = this.props
+    const { handleClick, state, id } = this.props
+
+    let cardContainer = {
+      transform: (this.props.state == 0) ? "rotateY(180deg)" : "rotateY(0deg)",
+      position: "relative",
+      width: "100%",
+      height: "100%",
+      transformStyle: "preserve-3d",
+      transition: "all 1s"
+    }
+
     return (
-      <div className='Card' onClick={() => { handleClick(id) }}>
-        <div
-          className={`Card-Content ${animation}`}
-        >
+      <div className='Card' onClick={() => { if (state != 2) { handleClick(id) } }}>
+        <div style={cardContainer}>
           <div className='Card-Front' style={this.frontStyle} >
           </div>
           <div className='Card-Back'>
