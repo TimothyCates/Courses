72bb1d31385d2610d5901780ca91c5be94cf0912
Timothy Cates <timothyccates1@gmail.com>
2022-06-23 10:29:42 -0700

    Made nad component

---
+++ Udemy/Adv Web Dev Bootcamp/Exercises/flag-guessing-app/src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+import "./Nav.css"
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav>
+        <h1>Guess the flag</h1>
+      </nav>
+    )
+  }
+}
+
+export default Nav
