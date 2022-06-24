8fdfc7573cf3aaddf9e3a22ce27e252acaa3f141
Timothy Cates <timothyccates1@gmail.com>
2022-06-23 17:29:10 -0700

    Setup master modules file that will export different models, referencing a todomodel not yet implemented

---
+++ Udemy/Adv Web Dev Bootcamp/Exercises/fullstack-todo/server/models/index.js
@@ -0,0 +1,10 @@
+let mongoose = require('mongoose')
+mongoose.set('debug', true)
+
+mongoose.connect('mongodb://localhost/fullstack-todo')
+
+mongoose.Promise = Promise;
+
+module.exports = {
+  Todo: require('./todo')
+}
