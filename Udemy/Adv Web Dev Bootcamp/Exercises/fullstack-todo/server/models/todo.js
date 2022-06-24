01d9c0297629562695c104662101aa7e4919342a
Timothy Cates <timothyccates1@gmail.com>
2022-06-23 17:44:05 -0700

    Created todo schema, with custom id property for use in react

--- Udemy/Adv Web Dev Bootcamp/Exercises/fullstack-todo/server/models/todo.js
+++ Udemy/Adv Web Dev Bootcamp/Exercises/fullstack-todo/server/models/todo.js
@@ -1,7 +1,24 @@
 const mongoose = require('mongoose')
+import {nanoid} from 'nanoid'
 
 const todoSchema = new mongoose.Schema({
-
+  id: {
+    type: String,
+    required: true,
+    default: () => nanoid(10)
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 let Todo = mongoose.model('Todo', todoSchema)
